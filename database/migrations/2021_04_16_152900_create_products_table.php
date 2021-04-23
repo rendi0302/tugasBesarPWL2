2cc7c609f7f7904c8769aa159453a54b0d7a9d08
rendi0302 <rendi.maulana0203@gmail.com>
2021-04-23 23:14:34 +0700

    membuat database

--- database/migrations/2021_04_16_152900_create_products_table.php
+++ database/migrations/2021_04_16_152900_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('qty');
+            $table->string('price');
+            $table->foreignId('brands_id')->constrained();
+            $table->foreignId('categories_id')->constrained();
             $table->string('photo');
-            $table->foreignId('roles_id')->constrained();
-            $table->rememberToken();
             $table->timestamps();
+
+
         });
     }
 
@@ -34,7 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('products');
     }
 }
